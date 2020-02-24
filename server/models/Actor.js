1e328c202b0cfbacf734de879243e223b404f749
D00219161 <57397919+D00219161@users.noreply.github.com>
2020-02-24 14:01:49 +0000

    Added Actors to db and updated files

---
+++ server/models/Actor.js
@@ -0,0 +1,39 @@
+const mongoose  = require('mongoose');
+const validator = require('validator');
+
+const SchemeConfig = {timestamps: true, skipVersioning: true};
+const ActorSchema  = new mongoose.Schema({
+
+    Name: {
+      type      : String,
+      required  : true,
+      validator : value => !validator.isEmpty(value)
+    },
+
+    DOB: {
+      type      : String,
+      required  : true,
+      validator : value => !validator.isEmpty(value)
+    },
+
+    Age: {
+      type      : String,
+      required  : true,
+      validator : value => !validator.isEmpty(value)
+    },
+
+    Nationality: {
+      type      : String,
+      required  : true,
+      validator : value => !validator.isEmpty(value)
+    },
+
+    Films: {
+      type      : String,
+      required  : true,
+      validator : value => !validator.isEmpty(value)
+    },
+    
+  }, SchemeConfig);
+
+  module.exports.Actor = mongoose.model('Actor', ActorSchema);
---
+++ server/routes/actors.js
@@ -0,0 +1,82 @@
+const express  = require('express');
+const router   = express.Router();
+const mongoose = require('mongoose'); // using to generate ObjectIDs
+const Actor     = require('../models/Actor').Actor;
+
+/**
+ * Functionality for this route:
+ *  C   POST    /Actors/       Create a new Actor
+ *  R   GET     /Actors         Gets an array of all Actors
+ *  R   GET     /Actors/:id     Get a single Actor, by ID
+ *  U   PUT     /Actors/:id     Update a single Actor, by id
+ *  D   DELETE  /Actors/:id     Delete a single Actor, by ID
+ */
+
+// GET an array of all Actors
+router.get('/', (req, res) => {
+    return mongoose
+      .model('Actor')
+      .find({})
+      .then (actors => res.json(actors))
+      .catch(err => res
+        .status(500)
+        .json({ok: false})
+      );
+  });
+
+  // GET a single Actor by ID
+router.get('/:id([0-9a-fA-F]{24})', (req, res) => {
+  return mongoose
+    .model('Actor')
+    .findOne({_id: req.params.id})
+    .then (actor => res.json(actor))
+    .catch(err => res
+      .status(500)
+      .json({ok: false})
+    );
+});
+
+// POST Create a new Actor
+router.post('/', (req, res) => {
+  return new Actor({
+    title     : req.body.title,
+  })
+  .save()
+  .then (actor => Actor.populate(actor, {path: '_id'}))
+  .then (actor => res.json(actor))
+  .catch(err => res
+    .status(400)
+    .json({ok: false, error: err.message})
+  );
+});
+
+// DELETE Delete a Actor with a given ID
+router.delete('/:id([0-9a-fA-F]{24})', (req, res) => {
+  return Actor
+    .deleteOne({_id: req.params.id})
+    .then (() => res.json({'ok': true}))
+    .catch(err => res
+      .status(500)
+      .json({ok: false})
+    );
+});
+
+// PUT Update a Actor
+router.put('/:id([0-9a-fA-F]{24})', (req, res) => {
+  return Actor
+    .findOneAndUpdate(
+      {_id: req.params.id},
+      {$set: {
+        title  : req.body.title,
+      }},
+      {new: true}
+    )
+    .then (film => Actor.populate(actor, {path: '_id'}))
+    .then (actor => res.json(actor))
+    .catch(err => res
+      .status(500)
+      .json({ok: false})
+    );
+});
+
+  module.exports = router;

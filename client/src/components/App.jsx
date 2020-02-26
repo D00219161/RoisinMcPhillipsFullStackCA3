8076fa3ffbba404c30724e37155529a5c88dde1d
D00219161 <57397919+D00219161@users.noreply.github.com>
2020-02-26 14:46:24 +0000

    added footer and updated data.json and app.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,16 +3,25 @@ import {Router} from "@reach/router";
 import Films   from './Films';
 import Film    from './Film';
 import AddFilm from './AddFilm';
-
+import Actors   from './Actors';
+import Actor    from './Actor';
+import AddActor from './AddActor';
+import Footer   from './Footer';
 class App extends React.Component {
 
   render() {
     return (
+      <div className='App'>
       <Router>
         <Films   path='/' />
         <Film    path='/film/:filmID' />
         <AddFilm path='/add-film/' />
+        <Actors   path='/' />
+        <Actor    path='/actor/:actorID' />
+        <AddActor path='/add-actor/' />
         </Router>
+        <Footer />
+        </div>
     );
   }
 

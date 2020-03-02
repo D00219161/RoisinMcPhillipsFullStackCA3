f0653fc22a9810f7c68d8266ca9301bac2241550
D00219161 <57397919+D00219161@users.noreply.github.com>
2020-03-02 13:06:48 +0000

    added nav bar

---
+++ client/src/components/Navagation.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Nav, Navbar} from 'react-bootstrap';
+
+class NavMenu extends Component {
+
+  render() {
+    return (
+      <Navbar>
+    <Navbar.Brand>Films</Navbar.Brand>
+    <Nav className="mr-auto">
+    <Nav.Link href="#films">Home</Nav.Link>
+      <Nav.Link href="#actors">Videos</Nav.Link>
+      <Nav.Link href="#directors">Contact</Nav.Link>
+    </Nav>
+  </Navbar>
+
+    );
+    
+  }
+
+}
+
+export default NavMenu;
